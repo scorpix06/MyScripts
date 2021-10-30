606254d4eafe1c21d51b526448cbb2e4d4503faf
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2021-10-30 15:49:29 +0200

    Create plex_server.sh

---
+++ plex_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+apt update
+apt install plexmediaserver
+sudo mkdir -p /opt/plexmedia/{movies,series}
+sudo chown -R plex: /opt/plexmedia
