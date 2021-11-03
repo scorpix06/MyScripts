769ded7902a72c199a5132e85b2863a9f7497357
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2021-11-03 14:08:51 +0100

    Update install_plex_server.sh

--- install_plex_server.sh
+++ install_plex_server.sh
@@ -9,7 +9,7 @@ apt update && apt upgrade -y
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 apt update
-apt install plexmediaserver -Y
+apt install plexmediaserver -y
 sudo mkdir -p /opt/plexmedia/{movies,series}
 sudo chown -R plex: /opt/plexmedia
 systemctl start plexmediaserver
