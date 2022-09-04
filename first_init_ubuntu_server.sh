d12989569f9dfbc39832841da004ba90a09a14e8
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2022-09-04 15:42:30 +0200

    Update first_init_ubuntu_server.sh

--- first_init_ubuntu_server.sh
+++ first_init_ubuntu_server.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 timezone = Europe/Paris
+ssh_gh_hostname = scorpix06
 
 if (( $EUID != 0 )); then
     echo "Please run as root"
     exit
 fi
+
+echo "[X] Importing ssh public key"
+ssh-import-id-gh $ssh_gh_hostname
 
 echo "[X] Changing timezone to " $timezone
 timedatectl set-timezone $timezone
