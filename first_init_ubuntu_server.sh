392e8a840a8ef6d62954372f0f99122de6da7f58
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2022-09-04 15:40:22 +0200

    Update first_init_ubuntu_server.sh

--- first_init_ubuntu_server.sh
+++ first_init_ubuntu_server.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+timezone = Europe/Paris
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+echo "[X] Changing timezone to " $timezone
+timedatectl set-timezone $timezone
+
+echo "[X] Enter the name of the server : "
+read name
+echo "[X] Changing hostname"
+
+hostnamectl set-hostname $name
+hostnamectl set-hostname $name --pretty
+
